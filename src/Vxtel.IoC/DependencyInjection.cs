877cb8eaa52e20fc2f249f1139938555b0d68ebf
leonardoavelar <leonardoavelar@terra.com.br>
2022-06-28 04:47:33 -0300

    Implementação da injeção de dependência dos services e repositórios

--- src/Vxtel.IoC/DependencyInjection.cs
+++ src/Vxtel.IoC/DependencyInjection.cs
@@ -11,10 +11,22 @@ namespace Vxtel.IoC
         public static void ConfigureDependencies(this IServiceCollection services)
         {
             // Repository
+            services.AddTransient<IChamadaRepository, ChamadaRepository>();
+            services.AddTransient<IClienteRepository, ClienteRepository>();
+            services.AddTransient<IConsumoRepository, ConsumoRepository>();
+            services.AddTransient<IContratoRepository, ContratoRepository>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
             services.AddTransient<ITarifaRepository, TarifaRepository>();
+            services.AddTransient<ITelefoneClienteRepository, TelefoneClienteRepository>();
 
             // Service
+            services.AddTransient<IChamadaService, ChamadaService>();
+            services.AddTransient<IClienteService, ClienteService>();
+            services.AddTransient<IConsumoService, ConsumoService>();
+            services.AddTransient<IContratoService, ContratoService>();
+            services.AddTransient<IProdutoService, ProdutoService>();
             services.AddTransient<ITarifaService, TarifaService>();
+            services.AddTransient<ITelefoneClienteService, TelefoneClienteService>();
         }
     }
 }
