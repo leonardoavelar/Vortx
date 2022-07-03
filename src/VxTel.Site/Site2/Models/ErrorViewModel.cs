443bfd3a4aaf77e498f7c8b5e0b85fb1751f4254
Leonardo Avelar <leonardoavelar@terra.com.br>
2022-07-03 19:09:15 -0300

    Delete src/VxTel.Site/Site2 directory

--- src/VxTel.Site/Site2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VxTel.Site.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
